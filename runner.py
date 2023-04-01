f065ba41a3c1b332a57e6397052a17977d4dc22a
Michael French <michael.x.french@gmail.com>
2023-04-01 14:21:59 -0500

    generate scenario succesfully

--- runner.py
+++ runner.py
@@ -5,7 +5,7 @@ import optparse
 import traci
 from sumolib import checkBinary
 
-from routes import generate_routefile
+from scenarios import generate_scenario
 
 if "SUMO_HOME" in os.environ:
     tools = os.path.join(os.environ["SUMO_HOME"], "tools")
@@ -17,17 +17,8 @@ def run():
     """execute the TraCI control loop"""
     step = 0
     # we start with phase 2 where EW has green
-    traci.trafficlight.setPhase("0", 2)
     while traci.simulation.getMinExpectedNumber() > 0:
         traci.simulationStep()
-        if traci.trafficlight.getPhase("0") == 2:
-            # we are not already switching
-            if traci.inductionloop.getLastStepVehicleNumber("0") > 0:
-                # there is a vehicle from the north, switch
-                traci.trafficlight.setPhase("0", 3)
-            else:
-                # otherwise try to keep green for EW
-                traci.trafficlight.setPhase("0", 2)
         step += 1
     traci.close()
     sys.stdout.flush()
@@ -53,8 +44,10 @@ if __name__ == "__main__":
     else:
         sumoBinary = checkBinary("sumo-gui")
 
-    generate_routefile()
+    name = 'test'
+    generate_scenario(name)
     traci.start(
-        [sumoBinary, "-c", "data/cross.sumocfg", "--tripinfo-output", "tripinfo.xml"],
+        [sumoBinary, "-c", f"data/{name}.sumocfg", "--tripinfo-output", "tripinfo.xml"],
     )
     run()
+    
---
+++ scenarios.py
@@ -0,0 +1,34 @@
+from networks import generate_network1
+from routes import generate_route1
+
+def generate_sumocfg(name: str):
+    with open(f'data/{name}.sumocfg', 'w') as f:
+        f.write(
+f"""<?xml version="1.0" encoding="UTF-8"?>
+
+<configuration xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/sumoConfiguration.xsd">
+
+    <input>
+        <net-file value="{name}.net.xml"/>
+        <route-files value="{name}.rou.xml"/>
+    </input>
+
+    <time>
+        <begin value="0"/>
+    </time>
+
+    <report>
+        <verbose value="true"/>
+        <no-step-log value="true"/>
+    </report>
+
+</configuration>
+""")
+
+def generate_scenario(name: str):
+    generate_network1(name)
+    generate_route1(name)
+    generate_sumocfg(name)
+
+if __name__ == "__main__":
+    generate_scenario('test')
