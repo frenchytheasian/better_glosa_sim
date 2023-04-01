d131b998e1c68d2fd57d2469afee5f523dbd77aa
Michael French <michael.x.french@gmail.com>
2023-04-01 14:06:10 -0500

    generate network and routes

---
+++ networks.py
@@ -0,0 +1,37 @@
+import subprocess
+
+def generate_nod1(name: str):
+    with open(f'data/{name}.nod.xml', 'w') as f:
+        f.write(
+f"""<?xml version="1.0" encoding="UTF-8"?>
+<nodes xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/nodes_file.xsd">
+    <node id="beg" x="0" y="0" type="priority" />
+    <node id="end" x="1000" y="0" type="priority" />
+
+""")
+        for i in range(1, 10):
+            f.write(f"""\t<node id="{i}" x="{i*100}" y="0" type="traffic_light" />\n""")
+
+        f.write("""</nodes>""")
+                
+def generate_edg1(name: str):
+    with open(f'data/{name}.edg.xml', 'w') as f:
+        f.write(
+f"""<?xml version="1.0" encoding="UTF-8"?>
+<edges xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/edges_file.xsd">
+
+""")
+        f.write(f"""\t<edge id="1r" from="beg" to="1" numLanes="1" priority="78" speed="13.89" />\n""")
+        for i in range(2, 10):
+            f.write(f"""\t<edge id="{i}r" from="{i-1}" to="{i}" numLanes="1" priority="78" speed="13.89" />\n""")
+        f.write(f"""\t<edge id="10r" from="9" to="end" numLanes="1" priority="78" speed="13.89" />\n""")
+
+        f.write("""</edges>""")
+
+def generate_network1(name: str):
+    generate_nod1(name)
+    generate_edg1(name)
+    subprocess.run(['netconvert', '-n', f'data/{name}.nod.xml', '-e', f'data/{name}.edg.xml', '-o', f'data/{name}.net.xml'])
+
+if __name__ == "__main__":
+    generate_network1('test')
--- routes.py
+++ routes.py
@@ -1,5 +1,25 @@
 import random
 
+def generate_route1(name: str):
+    N = 1000
+    demand = 1.0 / 10
+
+    with open(f'data/{name}.rou.xml', 'w') as f:
+        f.write(
+f"""<routes>
+    <vType id="type1" accel="0.8" decel="4.5" sigma="0.5" length="5" minGap="2.5" maxSpeed="16.67" guiShape="passenger"/>
+    <route id="right" edges="1r 2r 3r 4r 5r 6r 7r 8r 9r 10r" />
+""")
+
+        for i in range(N):
+            if random.uniform(0, 1) < demand:
+                f.write(
+                    f'\t<vehicle id="right_{i}" type="type1" route="right" depart="{i}" />\n'
+                )
+        
+        f.write("</routes>")
+    
+
 def generate_routefile():
     random.seed(42)  # make tests reproducible
     N = 3600  # number of time steps
@@ -43,3 +63,7 @@ guiShape="passenger"/>
                 )
                 vehNr += 1
         print("</routes>", file=routes)
+
+
+if __name__ == "__main__":
+    generate_route1('test')
